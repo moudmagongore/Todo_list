589a267eab66b6e5832ba7ff64918025e5ff9669
Mahmoud Diallo <moudmagongore@gmail.com>
2020-08-26 17:44:44 +0000

    mis à jour

--- app/Http/Controllers/CompteController.php
+++ app/Http/Controllers/CompteController.php
@@ -6,6 +6,13 @@ use Illuminate\Http\Request;
 
 class CompteController extends Controller
 {
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
     public function index()
     {
     	return view('comptes.create');
